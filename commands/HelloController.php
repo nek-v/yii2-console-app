1820a8fe1ce0bf6cdbd29a1a3e9a7e25ab11208e
nek <nek@srez.org>
2017-04-30 01:07:16 +0300

    Add Schedule Add phing build file

--- commands/HelloController.php
+++ commands/HelloController.php
@@ -3,11 +3,16 @@
 namespace app\commands;
 
 use yii\console\Controller;
+use yii\helpers\Console;
 
 class HelloController extends Controller {
 
-    public function actionIndex($message = 'hello world') {
-        echo $message . "\n";
+    public function actionIndex($message = 'Hello world') {
+        // color string
+        $this->stdout('Execute: ', Console::BOLD);
+        $this->stdout(realpath(__FILE__)."\r\n", Console::FG_YELLOW);
+        // 
+        echo $message."\r\n";
     }
 
 }
